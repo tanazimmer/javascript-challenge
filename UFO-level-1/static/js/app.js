53a6c31ce4ec8743057e54c827493fa6eb0ca2b4
tanazimmer <68484974+tanazimmer@users.noreply.github.com>
2020-11-01 21:10:22 -0500

    appended tr

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,28 +1,26 @@
 //call on data
-
+var tabledata = data;
+console.log(tabledata)
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the weather data from data.js
+// Console.log the UFO data from data.js
 console.log(data);
 
-// // Step 1: Loop Through `data` and console.log each weather report object
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-// });
+// // Step 1: Loop Through `data` and console.log each object
+data.forEach(function(UFOsightings) {
+    console.log(UFOsightings);
+});
 
-// // Step 2:  Use d3 to append one table row `tr` for each weather report object
+// // Step 2:  Use d3 to append one table row `tr` for each object
 // // Don't worry about adding cells or text yet, just try appending the `tr` elements.
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
-// });
+data.forEach(function(UFOsightings) {
+    console.log(UFOsightings);
+    var row = tbody.append("tr");
+});
 
-// // Step 3:  Use `Object.entries` to console.log each weather report value
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
+// // Step 3:  Use `Object.entries` to console.log each value
 
 //   Object.entries(weatherReport).forEach(function([key, value]) {
 //     console.log(key, value);
